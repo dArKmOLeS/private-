742df7d0cf6b32e1ac041e7d510c32e57f811fc7
dArKmOLeS <95976130+dArKmOLeS@users.noreply.github.com>
2024-06-13 18:14:47 +0530

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import os
 
-file_path = "https://github.com/dArKmOLeS/private-/blob/main/cities.txt"
+#file_path = "https://github.com/dArKmOLeS/private-/blob/main/cities.txt"
 
 
 def get_direction(degree):
@@ -12,41 +12,42 @@ def get_direction(degree):
     return directions[idx]
 
 
-def get_data():
-    try:
-        with open(file_path, "r") as file:
-            lines = file.readlines()
-    except FileNotFoundError:
-        stm.write("File not found. Please ensure the file exists.")
-    except Exception as e:
-        stm.write(f"An error occurred: {e}")
-    df = []
-    for i in lines:
-        line = list(i.split("|"))
-        df.append(line)
-    data_frame = pd.DataFrame(df, columns=["City", "Description"])
-    return data_frame
+# def get_data():
+#     try:
+#         with open(file_path, "r") as file:
+#             lines = file.readlines()
+#     except FileNotFoundError:
+#         stm.write("File not found. Please ensure the file exists.")
+#     except Exception as e:
+#         stm.write(f"An error occurred: {e}")
+#     df = []
+#     for i in lines:
+#         line = list(i.split("|"))
+#         df.append(line)
+#     data_frame = pd.DataFrame(df, columns=["City", "Description"])
+#     return data_frame
 
 
 def frontend():
     stm.title("WEATHER APPLICATION...")
-    data_frame = get_data()
-    cities = data_frame["City"]
+    # data_frame = get_data()
+    # cities = data_frame["City"]
+    cities = ["Ballia", "Varanasi", "Lucknow", "Jaipur", "Ahmedabad", "Chandigarh", "Amritsar", "Gorakhpur"]
     city = stm.selectbox("Select city :", cities, index=None)
     if city is not None:
         stm.write("Selected City : ", city)
         stm.divider()
-        index = 0
-        for i in data_frame["City"].values:
-            if city == i:
-                break
-            else:
-                index += 1
-        description = data_frame["Description"].at[index]
-        get_weather(city, description)
+        # index = 0
+        # for i in data_frame["City"].values:
+        #     if city == i:
+        #         break
+        #     else:
+        #         index += 1
+        # description = data_frame["Description"].at[index]
+        get_weather(city)
 
 
-def process_data(response, description):
+def process_data(response):
     data = response.json()
     if data['cod'] == 200:
         temperature = data["main"]["temp"]
@@ -66,17 +67,17 @@ def process_data(response, description):
             stm.write("Description : ", weather_description.upper())
             stm.write("Direction : ", wind_direction)
         stm.divider()
-        stm.write(description)
+        # stm.write(description)
     else:
         stm.write(f"Error fetching data: {response.status_code}")
 
 
-def get_weather(city, description):
-    api_key = "4cc73ace2556246080ce1af33ad35e79"
+def get_weather(city):
+    api_key = stm.secrets["api_key"]
     url = "http://api.openweathermap.org/data/2.5/weather"
     complete_url = f'{url}?q={city}&appid={api_key}&units=metric'
     response = requests.get(complete_url)
-    process_data(response, description)
+    process_data(responsen)
 
 
 frontend()
